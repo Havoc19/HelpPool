790e0732e1009dacd162ac1f8e94d51841c7af4a
Akarsh <akarshmaurya19@gmail.com>
2021-11-28 15:55:07 +0530

    Updates

---
+++ precache-manifest.2e55127315d00fb6f8e2bd125bbfc1fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c78d4280a4e1c506c4bd",
+    "url": "/HelpPool/static/css/main.9406f3ac.chunk.css"
+  },
+  {
+    "revision": "c78d4280a4e1c506c4bd",
+    "url": "/HelpPool/static/js/main.c78d4280.chunk.js"
+  },
+  {
+    "revision": "b125114498da79497f77",
+    "url": "/HelpPool/static/css/1.5a9086a4.chunk.css"
+  },
+  {
+    "revision": "b125114498da79497f77",
+    "url": "/HelpPool/static/js/1.b1251144.chunk.js"
+  },
+  {
+    "revision": "0cb72dd46699b2ead535",
+    "url": "/HelpPool/static/js/runtime~main.0cb72dd4.js"
+  },
+  {
+    "revision": "4efeaacdc3b36ffccfb360e240832211",
+    "url": "/HelpPool/index.html"
+  }
+];
